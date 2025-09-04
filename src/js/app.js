4a7d14dcff37fa3bffe905337a55cf1ec1d8fb99
yuliv <yuliv@YULI-LAP>
2025-09-03 20:59:46 -0600

    Commit Inicial

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+// Importar Bootstrap desde node_modules
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+// Tu propio código JS
+document.addEventListener("DOMContentLoaded", () => {
+});
