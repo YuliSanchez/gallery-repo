f62bae5b3af09db8a607757cf112ddcffb3e94f5
yuliv <yuliv@YULI-LAP>
2025-09-04 21:23:45 -0600

    Caso de uso: Mostrar información e imagen en Modal

--- src/js/app.js
+++ src/js/app.js
@@ -1,43 +1,83 @@
 // Importar Bootstrap desde node_modules
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
+import { Modal } from "bootstrap";
 import { paintings } from "./data/paintings";
 import { fer } from "./data/paintings";
 // Tu propio código JS
+var modal; 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log(paintings);
-    console.log(fer);
-    /**
-     * <div class="card shadow-sm h-100">
-            <img src="..." class="card-img-top" alt="paint image">
-            <div class="card-body">
-                <h5 class="card-title">painting.title</h5>
-                <p class="card-text mb-1"><strong>Autor:</strong> painting.author</p>
-                <p class="card-text"><strong>Año:</strong> painting.year</p>
-            </div>
-        </div>
-     */
-    let gallery = document.getElementById('gallery');
-    console.log(gallery);
+    renderGallery();
+    initModal();
+});
+
+function initModal(){
+    modal = new Modal('#gallery-modal', {
+        keyboard: false,
+        backdrop: true
+    })
+    
+    let elementModal = document.getElementById("gallery-modal");
+    elementModal.addEventListener('hidden.bs.modal', event => {
+  // do something...
+        modal._dialog.querySelector(".modal-body").innerHTML = "";
+    });
+}
+
+function renderGallery(){
+let gallery = document.getElementById('gallery');
     paintings.forEach((painting) => {
-        
-        console.log(painting);
+        let divColumn = document.createElement('div');
+        divColumn.classList.add('col-md-3');
+
         let div = document.createElement('div');
         div.classList.add('card', 'shadow-sm', 'h-100', 'main-gallery');
         let div2 = document.createElement('div');
-        div2.classList.add('card-body');
-
         div.appendChild(div2);
+
+        let image = document.createElement('img');
+        image.setAttribute("src", painting.img);
+        image.classList.add('card-img-top')
+        div2.appendChild(image);
+        div2.classList.add('card-body');
 
         let title = document.createElement('h5');
         title.innerText = painting.title;
+        title.classList.add('card-title');
         div2.appendChild(title);
-        console.log(div);
-        console.log(div2);
-        console.log(title);
-        gallery.appendChild(div);
 
-        
+        let author =document.createElement('p'); 
+        author.innerText = painting.author;
+        author.classList.add('card-text');
+        div2.appendChild(author); 
 
+         let year =document.createElement('p'); 
+        year.innerText = painting.year;
+        div2.appendChild(year);
+
+        divColumn.appendChild(div);
+        gallery.appendChild(divColumn);
+
+        //Subscripción a evento click
+        div.addEventListener("click",function(event){
+            onSelectPainting(painting); 
+        })
     });
-});
+}
+
+function onSelectPainting(painting) {
+    modal.show();
+    let modalBody = modal._dialog.querySelector(".modal-body");
+    modalBody.innerHTML = "";
+    const paintingCard = document.createElement("div");
+    paintingCard.className = "card mb-3";
+    paintingCard.innerHTML = `
+        <img src="${painting.img}" class="card-img-top" alt="${painting.title}">
+        <div class="card-body">
+          <h5 class="card-title">${painting.title}</h5>
+          <p class="card-text mb-1"><strong>Autor:</strong> ${painting.author}</p>
+          <p class="card-text mb-1"><strong>Description:</strong> ${painting.description}</p>
+          <p class="card-text"><strong>Año:</strong> ${painting.year}</p>
+        </div>
+    `;
+    modalBody.appendChild(paintingCard);
+}
